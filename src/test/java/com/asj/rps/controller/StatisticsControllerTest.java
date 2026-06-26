817ab560211049dd81e84a246d719c01a910a01a
rav3n6 <asjd09@gmail.com>
2026-06-26 21:39:40 +0400

    fix: update statistics MVC test for Spring Boot 4

--- src/test/java/com/asj/rps/controller/StatisticsControllerTest.java
+++ src/test/java/com/asj/rps/controller/StatisticsControllerTest.java
@@ -4,8 +4,8 @@ import com.asj.rps.dto.StatisticsResponse;
 import com.asj.rps.service.GameService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -19,7 +19,7 @@ class StatisticsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private GameService gameService;
 
     @Test
